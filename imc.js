8dc584a57a0b8b922f0864ecc3efcba8a57cd7c6
GuilhermeVeigaPedromilo <guipedromilo@gmail.com>
2023-05-05 08:24:17 -0300

    Alterações 05/05/2023

--- imc.js
+++ imc.js
@@ -1,31 +1,31 @@
        var ano;
        var idade;
        var resultado;
+       var nome;
 
         function calcular(event) {
             event.preventDefault();
            
             ano = parseFloat(document.getElementById("ano").value);
 
-         idade = 2023-ano;
+            nome = document.getElementById("nome").value;
+            
+            idade = 2023-ano;
 
             console.log(ano);
 
             if (idade < 12) {
                 resultado = document.getElementById("resultado");
-                resultado.innerHTML = "<br/> Seu resultado foi: " + idade + "<br/> Criança";
+                resultado.innerHTML = "<br>" + nome + " sua idade é " + idade + "<br/> Você está na fase Criança";
             } else if (idade >= 12 && idade <= 21) {
                 resultado = document.getElementById("resultado");
-                resultado.innerHTML = "<br/> Seu resultado foi: " + idade + "<br/> Adolescente";
+                resultado.innerHTML = "<br/>" + nome + " sua idade é " + idade + "<br/> Você está na fase Adolescente";
             } else if (idade >= 21 && idade <= 65) {
                 resultado = document.getElementById("resultado");
-                resultado.innerHTML = "<br/> Seu resultado foi: " + idade + "<br/> Adulto";
+                resultado.innerHTML = "<br/>" + nome + " sua idade é " + idade + "<br/> Você está na fase Adulto";
             } else if (idade >= 65) {
                 resultado = document.getElementById("resultado");
-                resultado.innerHTML = "<br/> Seu resultado foi: " + idade + "<br/> Idoso";
+                resultado.innerHTML = "<br/>" + nome + " sua idade é " + idade + "<br/> Você está na fase Idoso";
         }
-		
-		document.getElementById("peso").value = "";
-		document.getElementById("altura").value = "";
            
     }
